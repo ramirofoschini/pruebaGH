893418655b953d45ae2d70c2ec754ef6ac860a80
Martín Alejandro Estrada Saavedra <martin996@hotmail.com.ar>
2024-01-10 09:43:12 -0300

    a

---
+++ src/main/java/com/sa/demo/PruebaGhApplication.java
@@ -0,0 +1,13 @@
+package com.sa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaGhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaGhApplication.class, args);
+	}
+
+}
